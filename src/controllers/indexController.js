b144c32def60ae6dffd44872df5ac09b4b4ebbe1
Rodrigo Riveros <riverosdev@gmail.com>
2023-08-22 20:27:48 -0300

    agrego home dinamico

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsRouting = path.join(__dirname, '../data/products.json');
+const hotelsRouting = path.join(__dirname, '../data/hotels.json');
+const products = JSON.parse(fs.readFileSync(productsRouting, 'utf-8'));
+const hotels = JSON.parse(fs.readFileSync(hotelsRouting, 'utf-8'));
+
 module.exports = {
     index : (req, res) => {
-        return res.render('index');
-      }
+        return res.render('index', {
+          products,
+          hotels
+        });
+      },
+    search: (req, res) => {
+      const keywords = req.query.keywords
+      const results= products.filter(product => product.place.toLowerCase().includes(keywords.toLowerCase()))
+      res.render('results',{
+        results,
+        keywords
+      })
+    }
 }
