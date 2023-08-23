fd5dd3d6ada05393f29710462cd32f5c3076ade5
Rodrigo Riveros <riverosdev@gmail.com>
2023-08-22 23:33:38 -0300

    Guardando cambios locales antes de fusionar

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsRouting = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsRouting, 'utf-8'));
+
 module.exports = {
     index : (req, res) => {
         return res.render('index');
-      }
+      },
+    search: (req, res) => {
+      const keywords = req.query.keywords
+      const results= products.filter(product => product.place.toLowerCase().includes(keywords.toLowerCase()))
+      res.render('results',{
+        results,
+        keywords
+      })
+    }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { index } = require('../controllers/indexController');
+const { index, search } = require('../controllers/indexController');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', index );
+router.get('/search', search); 
 
 module.exports = router;
