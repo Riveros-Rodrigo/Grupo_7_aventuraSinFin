88e0694ddce2caa3d883c2a8e33210652fc5eb52
ClaraZalazar <zalazarmariaclara@gmail.com>
2023-09-08 21:23:56 -0300

    subo cambios en perfil de usuario

--- src/middlewares/cookieCheck.js
+++ src/middlewares/cookieCheck.js
@@ -1,5 +1,5 @@
 module.exports = (req,res,next) => {
-    if(req.cookies.craftsyForEver20){
+    if(req.cookies.aventuraSinFin){
         req.session.userLogin = req.cookies.aventuraSinFin
     }
     next()
