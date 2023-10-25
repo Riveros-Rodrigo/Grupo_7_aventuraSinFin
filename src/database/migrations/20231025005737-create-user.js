1b2944dfa0ab464c663b0643d78b5ac6c0b25950
ClaraZalazar <zalazarmariaclara@gmail.com>
2023-10-24 23:37:33 -0300

    correcciones de sprint anterior y migraciones de usuarios

---
+++ src/database/migrations/20231025005737-create-user.js
@@ -0,0 +1,63 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      surname: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      rolId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model : {
+            tableName : 'Rols'
+          }
+        }
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      seat: {
+        type: Sequelize.STRING
+      },
+      subscription: {
+        type: Sequelize.STRING
+      },
+      profilePhoto: {
+        type: Sequelize.STRING
+      },
+      birthday: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
