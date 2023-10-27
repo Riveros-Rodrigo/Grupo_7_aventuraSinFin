de68ed5368980ab717944c0c6575bad2607421ac
ClaraZalazar <zalazarmariaclara@gmail.com>
2023-10-26 21:03:53 -0300

    subo correcciones

--- src/controllers/products/remove.js
+++ src/controllers/products/remove.js
@@ -1,24 +1,3 @@
-// const db = require('../../database/models')
-// const path = require("path");
-
-// const controllerDelete = {
-
-// remove: (req, res) => {
-//     db.Product.destroy({
-// 		where : {
-// 			id : req.params.id
-// 		}
-// 	}).then(response => {
-// 		console.log(response);
-// 		return res.redirect("/dashboard");
-// 	})
-// 	.catch((error) => console.log(error));
-    
-//   },
-// };
-
-// module.exports = controllerDelete
-
 const { readJSON, writeJSON } = require("../../data")
 
 module.exports = (req,res) => {
@@ -29,5 +8,5 @@ module.exports = (req,res) => {
 
     writeJSON(productsModify, 'products.json')
 
-    return res.redirect('/admin')
+    return res.redirect('/dashboard')
 }
