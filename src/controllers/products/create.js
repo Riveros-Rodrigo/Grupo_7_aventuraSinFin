1061e20840f868e97ba1d400a6bb7b6059fc4c26
fabiCoseglia <fabiancoseglia@gmail.com>
2023-12-06 22:56:37 -0300

    actualizo products CREATE

--- src/controllers/products/create.js
+++ src/controllers/products/create.js
@@ -2,7 +2,49 @@ const {validationResult} = require('express-validator');
 const {existsSync, unlinkSync} = require('fs')
 const db = require('../../database/models');
 
-module.exports = (req, res) => {
+module.exports = async (req,res) =>{
+  const errors = validationResult(req);
+
+  if(errors.isEmpty()){
+    const {country,hotelId,description,price,discount,name} = req.body;
+
+    //creo el nuevo producto en la base de dato.
+    //usando async y await nos evitamos llenar el codigo de promesas, de esta manera nos evitamos la anidacion de .then()
+    
+    await db.Product.create({
+      name,
+      countryId: country ? +country : null,
+      hotelId : hotelId ? +hotelId : null,
+      description : description.trim(),
+      price,
+      discount : discount || 0,
+      image : req.file ? req.file.filename : null
+    });
+    return res.redirect('/dashboard')
+
+  }else{
+    //hacemos los llamados correspondientes para renderizar la vista productAdd.
+    const hotels = await db.Hotel.findAll({
+      order : ['name']
+    });
+
+    const countries = await db.Countrie.findAll({
+      order : ['name']
+    });
+
+    return res.render("productAdd", {
+      hotels,
+      countries,
+      errors : errors.mapped(),
+      old : req.body
+    });
+  }
+}
+
+
+//VERSION CON PROMESAS .then()
+/* module.exports = (req, res) => {
+
     
     const errors = validationResult(req);
 
@@ -65,4 +107,4 @@ module.exports = (req, res) => {
         .catch(error => console.log(error))
     }
   
-  }
+  } */
