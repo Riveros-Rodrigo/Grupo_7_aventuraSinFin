8a7b896f43516a88f13228eed5a2629005379b5f
fabiCoseglia <fabiancoseglia@gmail.com>
2023-12-07 23:43:19 -0300

    fix: createHotel.js

--- src/controllers/products/createHotel.js
+++ src/controllers/products/createHotel.js
@@ -3,16 +3,18 @@ const db = require('../../database/models')
 module.exports = async(req, res) => {
     
    try {
-    const {hotelId,country,name,description,price,discount} = req.body
-    const newHotel = await db.Hotel.create({
-        hotelId,
-        countryId: country ? +country : null,
+    const {hotelId,name,description,price,discount} = req.body
+
+    await db.Product.create({
+        hotelId : +hotelId,
+        countryId: null,
         name,
         description : description.trim(),
         price,
         discount : discount || 0,
         image : req.file ? req.file.filename : null
     });
+
     return res.redirect('/dashboard')
    } catch (error) {
     console.log(error);
