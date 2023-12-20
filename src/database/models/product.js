e5fbd455b0a17111c7eaafe442dba1a85e28d933
Eric <menaericdaniel@gmail.com>
2023-12-20 15:21:36 -0300

    fix: models & migrations

--- src/database/models/product.js
+++ src/database/models/product.js
@@ -11,10 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
      static associate(models) {
        // define association here
-       Product.belongsTo(models.Hotel, {
-         as : 'hotel',
-         foreignKey : 'hotelId'
-       });
+  
 
      Product.belongsTo(models.Countrie, {
        as : 'countrie',
