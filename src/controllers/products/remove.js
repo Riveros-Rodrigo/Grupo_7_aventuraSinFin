08918153c5e77f222322f5f421e6853a8adabea4
Rodrigo Riveros <riverosdev@gmail.com>
2023-12-21 12:45:28 -0300

    advance remove

--- src/controllers/products/remove.js
+++ src/controllers/products/remove.js
@@ -8,8 +8,14 @@ module.exports = async (req, res) => {
     const productToDelete = await db.Product.findByPk(id);
 
     if(productToDelete){
+
+      // Buscar y eliminar registros en la tabla hija (flights) relacionados con el producto
+      await db.Flight.destroy({ where: { productId: id } });
+
+      // Eliminar la imagen del producto, si existe
         existsSync(`./public/images/${productToDelete.image}`) &&
         unlinkSync(`./public/images/${productToDelete.image}`);
+        // Finalmente, eliminar el producto
         await productToDelete.destroy();
         
         return res.redirect("/dashboard");
